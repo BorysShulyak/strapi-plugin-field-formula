f7c19b28ac79da4bbc178cf4b892a706ca14073a
boris.shulyak <borysshulyak@gmail.com>
2023-08-05 13:41:22 +0300

    feat(server): delete unused services

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,7 @@
 'use strict';
 
-const myController = require('./my-controller');
 const mathjsController = require('./mathjs-controller');
 
 module.exports = {
-  myController,
   mathjsController
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,5 @@
 module.exports = [
   {
-    method: 'GET',
-    path: '/',
-    handler: 'myController.index',
-    config: {
-      policies: []
-    }
-  },
-  {
     method: 'POST',
     path: '/mathjs-calculate',
     handler: 'mathjsController.calculate',
